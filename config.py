cb33de5ac44d2ec66a8b1edab5c3138668c66511
kv566056-ui <kv566056@gmail.com>
2025-11-17 11:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,25 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
 from os import getenv
 from time import time
 from dotenv import load_dotenv
 
+# Load .env safely
 try:
     load_dotenv("config.env")
 except:
     pass
 
-    if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
-        print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
-        exit(1)
+# Validate BOT_TOKEN
+if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
+    print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
+    exit(1)
 
-    if (
-        not getenv("SESSION_STRING")
-        or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
-    ):
-        print("Error: SESSION_STRING must be set with a valid string")
-        exit(1)
-
+# Validate SESSION_STRING
+if (
+    not getenv("SESSION_STRING")
+    or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
+):
+    print("Error: SESSION_STRING must be set with a valid string")
+    exit(1)
 
 # Pyrogram setup
 class PyroConf(object):
@@ -32,4 +31,4 @@ class PyroConf(object):
 
     MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
     BATCH_SIZE = int(getenv("BATCH_SIZE", "10"))
-    FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "3"))
+    FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "2"))
